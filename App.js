fc3608a6960569829ac6061f358ee65c63d73d44
kdgyimah <kgyimah.dev@gmail.com>
2023-09-28 04:03:31 +0000

    config: add README and update landing page

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.asset}>
+        <Image source={require("./assets/bitcoin.webp")} style={styles.icon} />
+        <View style={{ marginLeft: 12 }}>
+          <Text style={{ ...styles.text, ...styles.assetTitle }}>Bitcoin</Text>
+          <View style={{ ...styles.row }}>
+            <View style={styles.assetRankContainer}>
+              <Text style={styles.assetRankText}>1</Text>
+            </View>
+            <View style={{ ...styles.text, ...styles.assetSubText }}>
+              <Text style={{ ...styles.text, ...styles.assetSubText }}>
+                BTC _ 1,03 %
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +28,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#121212",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 60,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  asset: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+  },
+  assetTitle: {
+    fontWeight: "bold",
+    marginBottom: 10,
+    fontSize: 25,
+  },
+  assetSubText: {
+    fontWeight: "bold",
+    color: "grey",
+    fontSize: 20,
+  },
+  assetRankContainer: {
+    backgroundColor: "#303645",
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+  assetRankText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+  text: {
+    color: "white",
+  },
+  icon: {
+    height: 60,
+    width: 60,
   },
 });
---
+++ README.md
@@ -0,0 +1,15 @@
+# CryptoPriceTracker Mobile App
+
+CryptoPriceTracker Mobile App. Inspiration CoinMarketCap, CoinGecko, Binance
+
+## Setup
+
+1. Install needed dependencies
+
+- `yarn install` or `npm install`
+
+2. Run the application
+
+   - `yarn start` or `npm run start`
+
+3. Scan the QR code that will generate in the terminal with Expo Go (Android) or the Camera app (iOS) to preview the application
